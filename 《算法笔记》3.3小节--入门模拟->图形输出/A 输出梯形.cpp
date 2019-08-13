afc60f693f578d52cbba61f3efc96a03548ffb09
Miroier <45321964+Miroier@users.noreply.github.com>
2019-08-13 22:02:59 +0800

    Update A 输出梯形.cpp

--- 《算法笔记》3.3小节--入门模拟->图形输出/A 输出梯形.cpp
+++ 《算法笔记》3.3小节--入门模拟->图形输出/A 输出梯形.cpp
@@ -1,1 +1,22 @@
-
+#include "cstdio"
+int main()
+{
+    int h;
+    //我佛了，有多组测试数据题目您倒是说一声啊
+    while (scanf("%d", &h) != EOF)
+    {
+        int w = 2 * (h - 1);
+        int H = h;
+        for (int i = 0; i < h; i++, w -= 2, H += 2)
+        {
+            for (int j = 0; j < w; j++)
+                printf(" ");
+            for (int j = 0; j < H; j++)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
