bbf0cd808d8eaf670ceb0aa2329c86566efdd227
Miroier <45321964+Miroier@users.noreply.github.com>
2019-08-16 00:08:35 +0800

    Create A.cpp

---
+++ 《算法笔记》4.1小节--算法初步->排序/A.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF)
+	{
+		int a[n];
+		for (int i = 0; i < n; i++)
+		{
+			scanf("%d", &a[i]);
+		}
+		sort(a, a + n);
+		for (int i = 0; i < n; i++)
+		{
+			printf("%d ", a[i]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
