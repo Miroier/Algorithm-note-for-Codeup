8799291970754a84d31656427d2ddc87d07a18c9
Miroier <45321964+Miroier@users.noreply.github.com>
2019-08-24 16:32:23 +0800

    Add files via upload

---
+++ 《算法笔记》4.6小节--算法初步->two pointers/B.cpp
@@ -0,0 +1,51 @@
+#include "cstdio"
+const int maxn = 100010;
+void merge(int A[], int L1, int R1, int L2, int R2)
+{
+    int i = L1, j = L2;
+    int temp[maxn], index = 0;
+    while (i <= R1 && j <= R2)
+    {
+        if (A[i] <= A[j])
+            temp[index++] = A[i++];
+        else
+            temp[index++] = A[j++];
+    }
+    while (i <= R1)
+        temp[index++] = A[i++];
+    while (j <= R2)
+        temp[index++] = A[j++];
+    for (int k = 0; k < index; k++)
+        A[L1 + k] = temp[k];
+}
+void mergesort(int A[], int left, int right)
+{
+    if (left < right)
+    {
+        int mid = (left + right) / 2;
+        mergesort(A, left, mid);
+        mergesort(A, mid + 1, right);
+        merge(A, left, mid, mid + 1, right);
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n--)
+    {
+        int m;
+        scanf("%d", &m);
+        int a[m];
+        for (int i = 0; i < m; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        mergesort(a, 0, m - 1);
+        for (int i = 0; i < m; i++)
+        {
+            printf("%d\n", a[i]);
+        }
+    }
+    return 0;
+}
---
+++ 《算法笔记》4.6小节--算法初步->two pointers/D.cpp
@@ -0,0 +1,44 @@
+//和C一样
+#include "cstdio"
+int partition(int A[], int left, int right)
+{
+    int temp = A[left];
+    while (left < right)
+    {
+        while (left < right && A[right] >= temp)
+            right--;
+        A[left] = A[right];
+        while (left < right && A[left] <= temp)
+            left++;
+        A[right] = A[left];
+    }
+    A[left] = temp;
+    return left;
+}
+void quicksort(int A[], int left, int right)
+{
+    if (left < right)
+    {
+        int pos = partition(A, left, right);
+        quicksort(A, left, pos - 1);
+        quicksort(A, pos + 1, right);
+    }
+}
+int main()
+{
+    int n;
+    while (scanf("%d", &n) != EOF)
+    {
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        quicksort(a, 0, n - 1);
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d\n", a[i]);
+        }
+    }
+    return 0;
+}
